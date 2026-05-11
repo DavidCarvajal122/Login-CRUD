63804c6325e83a7fc03d93c7e478a7563f367944
DavidCarvajal122 <david.carvajal.delrosario@udla.edu.ec>
2026-05-10 23:43:08 -0500

    fix: URL apunta a render

--- frontend/src/app/core/services/auth.service.ts
+++ frontend/src/app/core/services/auth.service.ts
@@ -10,7 +10,7 @@ import { LoginResponse } from '../../features/auth/models/login-response.model';
   providedIn: 'root'
 })
 export class AuthService {
-  private apiUrl = 'http://localhost:3000/api/auth';
+  private apiUrl = 'https://login-crud-m5ez.onrender.com/api/auth';
   private platformId = inject(PLATFORM_ID);
 
   constructor(private http: HttpClient) {}
--- frontend/src/app/core/services/user.service.ts
+++ frontend/src/app/core/services/user.service.ts
@@ -9,7 +9,7 @@ import { User } from '../../features/auth/models/user.model';
   providedIn: 'root'
 })
 export class UserService {
-  private apiUrl = 'http://localhost:3000/api/users';
+  private apiUrl = 'https://login-crud-m5ez.onrender.com/api/users';
 
   constructor(
     private http: HttpClient,
